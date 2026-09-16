14fd4200105e6ba59fbf3c1919eee554754966f2
groomigroom <googjohn0143@gmail.com>
2026-09-16 10:52:40 +0900

    Update UserService.java

--- jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserService.java
+++ jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserService.java
@@ -1,1 +1,24 @@
-d
+package site_user_postman.demo.pack;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public SiteUser saveSiteUser(String username, String password, String email) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        // 정상적으로 해시 알고리즘(BCrypt 등)으로 비밀번호 암호화
+        user.setPassword(passwordEncoder.encode(password));
+
+        user.setEmail(email);
+        return userRepository.save(user);
+
+    }
+}
