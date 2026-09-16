01653c1279091dccd1e4244a5c9349d654bfff24
groomigroom <googjohn0143@gmail.com>
2026-09-16 10:37:06 +0900

    Update DemoApplication.java

--- jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/DemoApplication.java
+++ jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/DemoApplication.java
@@ -1,1 +1,13 @@
-d
+package site_user_postman.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
