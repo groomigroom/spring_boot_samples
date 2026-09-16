ae9a305cd3c1db38fb949c214c8b5b0ec24cd3d0
groomigroom <googjohn0143@gmail.com>
2026-09-16 10:50:29 +0900

    Update UserJoinRequest.java

--- jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserJoinRequest.java
+++ jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserJoinRequest.java
@@ -1,1 +1,12 @@
-d
+package site_user_postman.demo.pack;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserJoinRequest {
+    private String username;
+    private String password;
+    private String email;
+}
