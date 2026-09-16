ab99f8ae7731acddcda7d59e412976919c37633c
groomigroom <googjohn0143@gmail.com>
2026-09-16 10:48:30 +0900

    Implement UserController for user creation
    Added UserController to handle user creation requests.

--- jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserController.java
+++ jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserController.java
@@ -1,1 +1,20 @@
-d
+package site_user_postman.demo.pack;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/users")
+    public SiteUser createUser(@RequestBody UserJoinRequest request) {
+        return userService.saveSiteUser(request.getUsername(), request.getPassword(), request.getEmail());
+    }
+
+}
