b3d6f826b6173b87f286a1433141ed18346ffe5c
groomigroom <googjohn0143@gmail.com>
2026-09-16 10:52:13 +0900

    Update UserRepository.java

--- jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserRepository.java
+++ jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/UserRepository.java
@@ -1,1 +1,9 @@
-d
+package site_user_postman.demo.pack;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<SiteUser, String> {
+
+}
