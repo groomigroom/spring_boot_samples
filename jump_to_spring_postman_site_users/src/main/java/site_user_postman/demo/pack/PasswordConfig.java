b400b5e45a7643c39214a10e3fe377a037f2710a
groomigroom <googjohn0143@gmail.com>
2026-09-16 10:40:27 +0900

    Update PasswordConfig.java

--- jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/PasswordConfig.java
+++ jump_to_spring_postman_site_users/src/main/java/site_user_postman/demo/pack/PasswordConfig.java
@@ -1,1 +1,21 @@
-d
+package site_user_postman.demo.pack;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig { // 오직 비밀번호 암호화 빈만 등록하는 역할
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
